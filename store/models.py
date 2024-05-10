fa63a53450ac4893afa917605148c19fcbe99a57
Marco TB <alvinmarco8@gmail.com>
2024-05-10 16:17:50 +0700

    update

--- store/models.py
+++ store/models.py
@@ -18,20 +18,45 @@ class Product (models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ""
+        return url
+
 class Order (models.Model):
     customer = models.ForeignKey("Customer", on_delete=models.SET_NULL, blank=True, null=True)
     date_order = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
-    transaction_id = models.CharField(max_length=200, null=True)
+    transaction_id = models.IntegerField(null=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
+    
+    @property
+    def get_cart_total (self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.totalPrice for item in orderitems])
+        return total
+    
+    @property   
+    def get_cart_items (self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 class OrderItem(models.Model):
     product = models.ForeignKey("Product", on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey("Order", on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property   
+    def totalPrice(self):
+        price = self.product.price * self.quantity
+        return price
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey("Customer", on_delete=models.SET_NULL, blank=True, null=True)

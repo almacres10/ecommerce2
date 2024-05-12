6652aa18642d9356d6d1d4f4d5526046942afb35
Marco TB <alvinmarco8@gmail.com>
2024-05-12 20:29:29 +0700

    update

--- static/js/cart.js
+++ static/js/cart.js
@@ -11,6 +11,24 @@ for(let i = 0; i<updateBtns.length; i++){
     if(user === "AnonymousUser"){
         console.log("Not log in")
     }else{
+        updateUserOrder()
+    }
+
+    function updateUserOrder(productId, action){
         console.log("User log in, sending data...")
+        let url = '/update_item/';
+        fetch(url, {
+            method : 'POST',
+            headers : {'Content-Type':'application/json'},
+            body:JSON.stringify({'productId' : productId, 'action' : action})
+        })
+
+        .then((response) =>{
+            return(response.json);
+        })
+
+        .then((data) => {
+            return(response.data);
+        })
     }
 }
